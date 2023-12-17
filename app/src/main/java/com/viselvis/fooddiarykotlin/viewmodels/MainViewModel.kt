5de3632f1661c120fad5b66ab13031cc887c4621
Elvis Antero <elvis.antero@veritaspay.com>
2023-12-17 16:25:12 +0800

    Modifications in home fragment

--- app/src/main/java/com/viselvis/fooddiarykotlin/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/viselvis/fooddiarykotlin/viewmodels/MainViewModel.kt
@@ -1,16 +1,31 @@
 package com.viselvis.fooddiarykotlin.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.*
 import com.viselvis.fooddiarykotlin.database.FoodItemModel
 import com.viselvis.fooddiarykotlin.database.FoodItemRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 
 class MainViewModel(private val repo: FoodItemRepository): ViewModel() {
-    val allFoodItems: LiveData<List<FoodItemModel>> = repo.firstThreeFoodItems.asLiveData()
+    // val allFoodItems: LiveData<List<FoodItemModel>> = repo.firstThreeFoodItems.asLiveData()
+    private val viewModelState = MutableStateFlow(MainViewModelState(latestFoodItems = emptyList()))
+    val uiState = viewModelState
+
+    init {
+        viewModelScope.launch {
+            repo.firstThreeFoodItems.collect { items ->
+                viewModelState.update { it.copy(latestFoodItems = items) }
+            }
+        }
+    }
 }
+
+data class MainViewModelState(
+    val latestFoodItems: List<FoodItemModel>? = null
+)
 
 class MainViewModelFactory(private val repo: FoodItemRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
