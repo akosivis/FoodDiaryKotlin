7d85336b651c5847c66d1ae6dbdcd2c5e060e05d
Elvis Antero <elvis.antero@veritaspay.com>
2024-01-27 16:00:06 +0800

    Implementation of writing and retrieving username from DataStore Preferences and other modifications

--- app/src/main/java/com/viselvis/fooddiarykotlin/viewmodels/EnterUsernameViewModel.kt
+++ app/src/main/java/com/viselvis/fooddiarykotlin/viewmodels/EnterUsernameViewModel.kt
@@ -4,32 +4,71 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.viselvis.fooddiarykotlin.MainActivity
+import com.viselvis.fooddiarykotlin.database.FoodItemModel
 import com.viselvis.fooddiarykotlin.database.FoodItemRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
+import java.util.*
 
-class EnterUsernameViewModel: ViewModel() {
-    var userName by mutableStateOf("")
+class EnterUsernameViewModel(private val repo: FoodItemRepository): ViewModel() {
+    private var _uiState = MutableStateFlow(
+        EnterUsernameState(
+            userName = "",
+            isUserNameSaved = false
+        )
+    )
+    val uiState: StateFlow<EnterUsernameState> = _uiState.asStateFlow()
 
-    fun enterUserName(input: String) {
-        userName = input
+    init {
+        viewModelScope.launch {
+            repo.usernameFlow.collect { name ->
+                _uiState.update {
+                    it.copy(
+                        userName = name,
+                        isUserNameSaved = name != ""
+                    )
+                }
+            }
+        }
+    }
+
+    fun updateUserName(input: String) {
+        if (input.trim().isNotEmpty()) {
+            _uiState.update {
+                it.copy (userName = input)
+            }
+        }
     }
 
     fun saveUserName() {
-
-    }
-
-    suspend fun writeUserName(input: String) {
-
+        val userNameTrimmed = _uiState.value.userName.trim()
+        if (userNameTrimmed.isNotEmpty()) {
+            viewModelScope.launch {
+                repo.writeUserName(userNameTrimmed)
+            }
+            _uiState.update {
+                it.copy(isUserNameSaved = true)
+            }
+        }
     }
 }
 
-class EnterUsernameViewModelFactory() : ViewModelProvider.Factory {
+data class EnterUsernameState (
+    var userName: String,
+    var isUserNameSaved: Boolean,
+)
+
+class EnterUsernameViewModelFactory(private val repo: FoodItemRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(EnterUsernameViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return EnterUsernameViewModel() as T
+            return EnterUsernameViewModel(repo) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
