08935c3b167cd2b2436c28bd1e2329860604d409
Elvis Antero <visntr2@gmail.com>
2024-10-02 19:08:18 +0800

    Initial changes for SettingRoute.kt

--- app/src/main/java/com/viselvis/fooddiarykotlin/ui/routes/SettingRoute.kt
+++ app/src/main/java/com/viselvis/fooddiarykotlin/ui/routes/SettingRoute.kt
@@ -1,9 +1,16 @@
 package com.viselvis.fooddiarykotlin.ui.routes
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.viselvis.fooddiarykotlin.utils.NotWorkingDisplayPage
 
 @Composable
@@ -11,6 +18,24 @@ fun SettingsRoute() {
     Surface (
         modifier = Modifier.fillMaxSize() 
     ) {
-        NotWorkingDisplayPage(message = "This feature is still being developed!")
+        // NotWorkingDisplayPage(message = "This feature is still being developed!")
+
+        Column (
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row (modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                Text(
+                    text = "Set reminder",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+
+            Row (modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                Text(
+                    text = "Set dark theme",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
     }
 }
