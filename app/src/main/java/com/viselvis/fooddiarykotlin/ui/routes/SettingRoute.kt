4dd60de1f52fd160bf634ae3cd4e5675d6e8aa5c
Elvis Antero <visntr2@gmail.com>
2024-10-10 19:15:27 +0800

    Followup changes for settings route and viewmodel

--- app/src/main/java/com/viselvis/fooddiarykotlin/ui/routes/SettingRoute.kt
+++ app/src/main/java/com/viselvis/fooddiarykotlin/ui/routes/SettingRoute.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,31 +16,46 @@ import com.viselvis.fooddiarykotlin.utils.NotWorkingDisplayPage
 
 @Composable
 fun SettingsRoute() {
+    val itemNumbers = listOf(0, 1)
+
     Surface (
         modifier = Modifier.fillMaxSize() 
     ) {
         // NotWorkingDisplayPage(message = "This feature is still being developed!")
-
-        Column (
-            modifier = Modifier.fillMaxSize()
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
         ) {
-            SettingsItem("Set reminder")
-            SettingsItem("Set dark theme")
+            items(itemNumbers.size) { itemNumber ->
+                SettingsItem(itemNumber = itemNumber)
+            }
         }
+//        Column (
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            SettingsItem("Set reminder")
+//            SettingsItem("Set dark theme")
+//        }
     }
 }
 
 @Composable
 fun SettingsItem (
-    text: String,
+    text: String = "",
     itemNumber: Int = 0
 ) {
     Row (modifier = Modifier.fillMaxWidth().padding(8.dp)) {
         Text(
-            text = text,
+            text = when (itemNumber) {
+                1 -> "Set dark theme"
+                else -> "Set reminder"
+            },
             style = MaterialTheme.typography.titleMedium
         )
 
-
+//        if (itemNumber) {
+//            1 -> {
+//
+//            }
+//        }
     }
 }
--- app/src/main/java/com/viselvis/fooddiarykotlin/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/com/viselvis/fooddiarykotlin/viewmodels/SettingsViewModel.kt
@@ -3,9 +3,20 @@ package com.viselvis.fooddiarykotlin.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.viselvis.fooddiarykotlin.database.FoodItemRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.util.Calendar
+
+data class SettingsUiState(
+    val isLoading: Boolean = true,
+    val savedReminderTime: String = "2030",
+    val isSetToDarkTheme : Boolean = false,
+)
 
 class SettingsViewModel(): ViewModel() {
-
+    private val currentSettingsUiState = MutableStateFlow(
+        SettingsUiState()
+    )
+    val uiState = currentSettingsUiState
 }
 
 class SettingsViewModelFactory  : ViewModelProvider.Factory {
